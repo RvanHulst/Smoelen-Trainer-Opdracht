edd6bb3c9505038992bf5427b4ab1396bb626f72
RvanHulst <99051006@mydavinci.nl>
2021-09-01 11:54:35 +0200

    Radio buttons added voor hoeveelheid mensen

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 startKnop.onclick = function () {
-    startPagina.style.display = "none";
+    startPagina.style.display = "block"; //verander terug naar none
     smoelenTrainer.style.display = "block";
+
+    var radiotButtonArray = document.getElementsByClassName('aantalMensen'); // haal radio buttons op bij class
+    console.log(radiotButtonArray[0].value);
 }
