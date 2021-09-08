3e1842a24f5affc01de2ec33a2dfe063fa90450d
RvanHulst <99051006@mydavinci.nl>
2021-09-08 11:07:29 +0200

    Shuffel function console logged nu de juiste volgorde en onthoud welke waar staat

--- js/main.js
+++ js/main.js
@@ -1,33 +1,31 @@
-startKnop.onclick = function () {
-    startPagina.style.display = "none"; //verander terug naar none
-    smoelenTrainer.style.display = "block";
     
-    gridItems()
-}
-var aantalMensen = document.getElementsByClassName('aantalMensen'); // haal radio buttons op bij class.
-
-for (let i = 0; i < aantalMensen.length; i++) {
-    console.log(aantalMensen)
-    if (aantalMensen[i].checked == true) {
-        console.log("het werkt")
-    }
-
-}
-
 const persoon = [
     {name: 'Persoon1', img: "fotopersoon1"},
     {name: 'Persoon2', img: "fotopersoon2"},
     {name: 'Persoon3', img: "fotopersoon3"},
     {name: 'Persoon4', img: "fotopersoon4"}
 ]
-
+    
 const naamPersoon = ['Persoon1', 'Persoon2', 'Persoon3', 'Persoon4'];
 const fotoPersoon = ['fotopersoon1', 'fotopersoon2', 'fotopersoon3', 'fotopersoon4'];
 
-/*for (var b = 0; b < naamPersoon.length; b++) {
-    console.log(naamPersoon[b]);
-}*/
+window.onload = function() {
+    var aantalMensen = document.getElementsByClassName('aantalMensen'); // haal radio buttons op bij class.
 
+    for (let i = 0; i < aantalMensen.length; i++) {
+        console.log(aantalMensen)
+        if (aantalMensen[i].checked == true) {
+            console.log("het werkt")
+        }
+    }
+}
+
+startKnop.onclick = function () {
+    startPagina.style.display = "none"; //verander terug naar none
+    smoelenTrainer.style.display = "block";
+    
+    gridItems()
+}
 
 function gridItems() {
    
@@ -45,11 +43,34 @@ function gridItems() {
 
         naam.innerHTML = naamPersoon[index];
         foto.innerHTML = fotoPersoon[index];
-    
+ 
+        naam.id = "persoonNaam" + [index];
+        foto.id = "persoonFoto" + [index];
+
         naam.classList.add("gridItem")
         foto.classList.add("gridItem")
-    
-    }
 
+        //run functie "itemOnclick"
+        //geef parameter [index] mee (Bijv op for loop 1 geeft hij "0" mee) 
+        itemOnclick([index]);
+    }
     console.log(fotoPersoon)
-  }
+}
+
+function itemOnclick(nummer){
+    var naam = document.getElementById('persoonNaam' + nummer);
+    var foto = document.getElementById('persoonFoto' + nummer);
+
+    naam.onclick = function () {
+        console.log(naam);
+    }
+    foto.onclick = function () {
+        console.log(foto);
+    }
+}
+
+
+// maak een onclick functie voor aanklikken van de namen en fotos
+// functie, wanneer je klikt run console log
+
+// zorgen dat de juiste namen console loggen op de juiste possitie (ook voor de foto's)
