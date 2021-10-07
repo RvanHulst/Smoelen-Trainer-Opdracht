56e45a974110d93b4c4e09817bb5bf4190abc719
RvanHulst <99051006@mydavinci.nl>
2021-10-07 14:01:07 +0200

    Compare function

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ const persoon = [
 
 var naamCompare;
 var fotoCompare;
+var lastClick;
 
 const naamPersoon = ['Persoon1', 'Persoon2', 'Persoon3', 'Persoon4'];
 const fotoPersoon = ['fotopersoon1', 'fotopersoon2', 'fotopersoon3', 'fotopersoon4'];
@@ -46,6 +47,7 @@ function gridItems() {
 
         naam.innerHTML = naamPersoon[index];
         foto.innerHTML = fotoPersoon[index];
+        //foto.src = "https://picsum.photos/200";
  
         naam.id = naamPersoon[index];
         foto.id = fotoPersoon[index];
@@ -63,66 +65,36 @@ function itemOnclick(naamNummer, fotoNummer){
     var naam = document.getElementById(naamNummer);
     var foto = document.getElementById(fotoNummer);
 
-    naam.onclick = function () { checkClick(this.id); }
-    foto.onclick = function () {
-        //fotoCompare = persoon.findIndex(findFoto => findFoto.img === foto.innerHTML);
-        //console.log(fotoCompare)
-        //compare();
-        checkClick(event);
-
-    }
+    naam.onclick = function () { checkClick(this); }
+    foto.onclick = function () { checkClick(this); }
 }
-
-var lastClick
 
 
 function checkClick(parameter){
-    console.log('The this: ', parameter);
-    //console.log(event.target.id)  
+    var lastChar = parameter.id.substr(parameter.id.length - 1);
+    var lastCharPrev = lastClick.id.substr(lastClick.id.length - 1);
 
-    //if statement die controleerd of last click niet leeg is EN of last click als die gelijk is aan de nieuwe event data atributte.
-    
-    
-    //zo ja match.
-   
-   
-    //zo nee maak leeg.
-    
-    // if in een if statment met de laatste een else
-    
-    
-    //  element.getAttribute('data-name');
-
-}
-
-
-
-
-
-
-
-
-
-// #3 Als gebruiker kan ik in de trainer een willekeurige volgorde één naam en één foto aanklikken. 
-// Als ze bij dezelfde persoon horen dan verdwijnen beide
-
-
-//functie maken de checked als ze bij elkaar horen
-// geeft een true als ze bij elkaar horen en geeft een false als ze  niet bij elkaar horen.
-
-
-
-// maak een if functie aan die Correct geeft als ze bij elkaar horen, 
-// maak een else functioe aan als die incorrect is als ze niet bij elkaar horen.
-
-
-function compare(){
-    if (naamCompare == fotoCompare) {
+    if (lastClick) {
+        //Kijk of de cijfers gelijk zijn van lastchar en lastchar prev
+        //Kijk of het div element hetzelfde is van lastClick en parameter
+        if (lastCharPrev == lastChar && lastClick != parameter) {
+            console.log("Geen joden")
+            lastClick = "";
         
-
+        }
+        else{
+            console.log('Filling lastclick because its empty: ', parameter);
+            lastClick = parameter;
+        }
     }
-    //Haal data op die je kan gebruiken met de "persoon" array
+    else{
+        console.log('Filling lastclick because its empty: ', parameter);
+        lastClick = parameter;
+    }
 
+        // Bestaat lastClick.
+        // Zo ja, ga dan vergelijken.
+        //zo niet vul hem op.
 
-// if array doorheen lopen en dan weer door heen lopen en dan vergelijken met de zelfde getalof naam
 }
+
