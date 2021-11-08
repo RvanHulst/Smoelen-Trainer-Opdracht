7e243d612833b6f9b24da6e534912c1fb1ff1637
Raaf <99051006@mydavinci.nl>
2021-11-08 15:51:34 +0100

    Compare function makkelijker gemaakt,  nieuwe punten functie, nieuwe array

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-    
-const persoon = [
-    {name: 'Persoon1', img: "fotopersoon1"},
-    {name: 'Persoon2', img: "fotopersoon2"},
-    {name: 'Persoon3', img: "fotopersoon3"},
-    {name: 'Persoon4', img: "fotopersoon4"}
-]
 var scoreGeschiedenis = []
 
 var naamArray = []
@@ -16,8 +9,20 @@ var lastClick;
 var goedCount = 0;
 var foutCount = 0;
 
-const naamPersoon = ['Persoon1', 'Persoon2', 'Persoon3', 'Persoon4', 'Persoon5', 'Persoon6', 'Persoon7', 'Persoon8', 'Persoon9', 'Persoon10', 'Persoon11'];
-const fotoPersoon = ['fotopersoon1', 'fotopersoon2', 'fotopersoon3', 'fotopersoon4', 'fotopersoon5', 'fotopersoon6', 'fotopersoon7', 'fotopersoon8', 'fotopersoon9', 'fotopersoon10', 'fotopersoon11'];
+var fotoPersoon = [
+ {name: 'BillCosby',source: 'https://media.nu.nl/m/yqqxcxcampsm_xwd640.jpg/bill-cosby-is-een-vrij-man-vijf-vragen-over-de-beslissing-van-de-rechter.jpg', punten: 0},
+ {name: 'DonaldTrump',source: 'https://images0.persgroep.net/rcs/7Lx4ekG65vqHR6cfLMui2aRjItc/diocontent/208413025/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8', punten: 0},
+ {name: 'BarrackObama', source: 'https://www.europa-nu.nl/9353000/1/j4nvh0qavhjkdqd_j9vvj9idsj04xr6/vlbjj03d1wny?sizew=933&sizeh=1200&crop=778:58:1081:1390&lm=qfo8io', punten: 0},
+ {name: 'JoeBiden', source: 'https://upload.wikimedia.org/wikipedia/commons/6/68/Joe_Biden_presidential_portrait.jpg', punten: 0},
+ {name: 'JoeExotic', source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Joe_Exotic_%28Santa_Rose_County_Jail%29.png/266px-Joe_Exotic_%28Santa_Rose_County_Jail%29.png', punten: 0},
+ {name: 'HillaryClinton', source: 'https://ichef.bbci.co.uk/news/1024/branded_news/1683A/production/_120681229_hillaryclintonatqueensbelfast005.jpg', punten: 0},
+ {name: 'GeorgeBush', source: 'https://images0.persgroep.net/rcs/XzH8uDIuWNNJynh-DQvEXmjLPXQ/diocontent/31665363/_crop/0/233/3496/1975/_fitwidth/763?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.8', punten: 0},
+ {name: 'JohnKennedy', source: 'https://www.biography.com/.image/t_share/MTgwNDM2ODA3NjQzNTcyMDc2/gettyimages-97347150.jpg', punten: 0},
+ {name: 'RobertOppenheimer', source: 'https://upload.wikimedia.org/wikipedia/commons/0/03/JROppenheimer-LosAlamos.jpg', punten: 0},
+ {name: 'TheodoreRoosevelt ', source: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/President_Roosevelt_-_Pach_Bros.jpg', punten: 0},
+ {name: 'HarryPotter', source: 'https://upload.wikimedia.org//wikipedia/en/d/d7/Harry_Potter_character_poster.jpg', punten: 0}
+];
+
 
 window.onload = function() {
     var aantalMensen = document.getElementsByClassName('aantalMensen'); // haal radio buttons op bij class.
@@ -71,7 +76,17 @@ blindeVlekKnop.onclick = function(){
     instellingenPagina.style.display = "none";
     geschiedenisPagina.style.display = "none";
     blindeVlekPagina.style.display = "block";
+    console.log(fotoPersoon.sort((a,b) => (a.punten < b.punten) ? 1 : ((b.punten < a.punten) ? -1 : 0)))
     for (let i = 0; i < array.length; i++) {
+        //zorgen dat alleen de 3 fouten zichtbaar zijn
+    
+       
+        // met een if statement het ophalen en zorgen dat die alleen de meeste punten qua fout laat zien.
+
+
+        if ( result.punten = 1)
+        var result = fotoPersoon.find( ({ name }) => name === parameter.id);
+        result.punten = result.punten + 1;
         
     }
 }
@@ -109,19 +124,22 @@ function gridItems() {
 
     for (let index = 0; index < persoonData; index++ ) {
         var naam = document.createElement("div");
-        var foto = document.createElement("div");
-    
-        naam.innerHTML = naamPersoon[index];
-        foto.innerHTML = fotoPersoon[index];
+        var fotoDiv = document.createElement("div");
+        var foto = document.createElement("img");
+
+        naam.innerHTML = fotoPersoon[index].name;
+        foto.src = fotoPersoon[index].source;
  
-        naam.id = naamPersoon[index];
-        foto.id = fotoPersoon[index];
+        naam.id = fotoPersoon[index].name;
+        fotoDiv.id = fotoPersoon[index].name;
 
         naam.classList.add("gridItem")
-        foto.classList.add("gridItem")
+        fotoDiv.classList.add("gridItem")
+
+        fotoDiv.appendChild(foto);
 
         naamArray.push(naam);
-        fotoArray.push(foto);
+        fotoArray.push(fotoDiv);
     }
     naamArray.sort(() => Math.random() - 0.5);
     fotoArray.sort(() => Math.random() - 0.5);
@@ -130,15 +148,17 @@ function gridItems() {
         gridContainer1.appendChild(naamArray[i]);
         gridContainer2.appendChild(fotoArray[i]);
 
+
         //run functie "itemOnclick"
         //geef parameter [index] mee (Bijv op for loop 1 geeft hij "0" mee) 
-        itemOnclick(naamArray[i].id, fotoArray[i].id);
+        itemOnclick(naamArray[i], fotoArray[i]);
     }
 }
 
 function matchGeschiedenis(){
-    scoreGeschiedenis.unshift({Goed: goedCount , Fout: foutCount, tijd: Date()});
+    scoreGeschiedenis.unshift({Goed: goedCount , Fout: foutCount, tijd: Date(), meesteFout: ""});
     console.log(scoreGeschiedenis);
+
 }
 
 function time(){
@@ -170,21 +190,16 @@ function time(){
 }
 
 function itemOnclick(naamNummer, fotoNummer){
-    var naam = document.getElementById(naamNummer);
-    var foto = document.getElementById(fotoNummer);
 
-    naam.onclick = function () { checkClick(this); }
-    foto.onclick = function () { checkClick(this); }
+    naamNummer.onclick = function () { checkClick(this); }
+    fotoNummer.onclick = function () { checkClick(this); }
 }
 
 function checkClick(parameter){
-    var lastChar = parameter.id.substr(parameter.id.length - 1);
-    if (lastClick) var lastCharPrev = lastClick.id.substr(lastClick.id.length - 1);  
-
     if (lastClick) {
         //Kijk of de cijfers gelijk zijn van lastchar en lastchar prev
         //Kijk of het div element hetzelfde is van lastClick en parameter
-        if (lastCharPrev == lastChar && lastClick != parameter) {
+        if (lastClick.id == parameter.id && lastClick != parameter) {
             console.log("Het is een match")
             parameter.style.display = "none";
             lastClick.style.display = "none";
@@ -193,10 +208,15 @@ function checkClick(parameter){
             document.getElementById("pointsGoed").innerHTML = goedCount + "Heeft u er goed";
         }
         else{
-            console.log('Hij is fout ', parameter);
+            
+            console.log('Hij is fout ', parameter.id);
             lastClick = "";
             foutCount = foutCount + 1;
             document.getElementById("pointsFout").innerHTML = foutCount  + "Heeft u er fout";
+            var result = fotoPersoon.find( ({ name }) => name === parameter.id);
+            result.punten = (result.punten + 1);
+            console.log(fotoPersoon);
+
         }
     }
     else{
