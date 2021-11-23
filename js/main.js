097d8c8c65c7bd00b2bea1acf58ae619a9a37f35
RvanHulst <99051006@mydavinci.nl>
2021-11-23 15:10:52 +0100

    Updated naming conventions

--- js/main.js
+++ js/main.js
@@ -1,109 +1,100 @@
-var scoreGeschiedenis = []
+var scoreHistory = []
 
-var naamArray = []
-var fotoArray = []
-var blindeVlekPunten = []
+var names = []
+var photos = []
+var blindeVlekPoints = []
+var backButtons = Array.from(document.getElementsByClassName("backButton"));
 
-var naamCompare;
-var fotoCompare;
 var lastClick;
-var goedCount = 0;
-var foutCount = 0;
+var correctAnswersCounter = 0;
+var incorrectAnswersCounter = 0;
 
-var fotoPersoon = [
- {name: 'BillCosby',source: 'https://media.nu.nl/m/yqqxcxcampsm_xwd640.jpg/bill-cosby-is-een-vrij-man-vijf-vragen-over-de-beslissing-van-de-rechter.jpg', punten: 0},
- {name: 'DonaldTrump',source: 'https://images0.persgroep.net/rcs/7Lx4ekG65vqHR6cfLMui2aRjItc/diocontent/208413025/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8', punten: 0},
- {name: 'BarrackObama', source: 'https://www.europa-nu.nl/9353000/1/j4nvh0qavhjkdqd_j9vvj9idsj04xr6/vlbjj03d1wny?sizew=933&sizeh=1200&crop=778:58:1081:1390&lm=qfo8io', punten: 0},
- {name: 'JoeBiden', source: 'https://upload.wikimedia.org/wikipedia/commons/6/68/Joe_Biden_presidential_portrait.jpg', punten: 0},
- {name: 'JoeExotic', source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Joe_Exotic_%28Santa_Rose_County_Jail%29.png/266px-Joe_Exotic_%28Santa_Rose_County_Jail%29.png', punten: 0},
- {name: 'HillaryClinton', source: 'https://ichef.bbci.co.uk/news/1024/branded_news/1683A/production/_120681229_hillaryclintonatqueensbelfast005.jpg', punten: 0},
- {name: 'GeorgeBush', source: 'https://images0.persgroep.net/rcs/XzH8uDIuWNNJynh-DQvEXmjLPXQ/diocontent/31665363/_crop/0/233/3496/1975/_fitwidth/763?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.8', punten: 0},
- {name: 'JohnKennedy', source: 'https://www.biography.com/.image/t_share/MTgwNDM2ODA3NjQzNTcyMDc2/gettyimages-97347150.jpg', punten: 0},
- {name: 'RobertOppenheimer', source: 'https://upload.wikimedia.org/wikipedia/commons/0/03/JROppenheimer-LosAlamos.jpg', punten: 0},
- {name: 'TheodoreRoosevelt ', source: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/President_Roosevelt_-_Pach_Bros.jpg', punten: 0},
- {name: 'HarryPotter', source: 'https://upload.wikimedia.org//wikipedia/en/d/d7/Harry_Potter_character_poster.jpg', punten: 0}
+var persons = [
+ {name: 'BillCosby',source: 'https://media.nu.nl/m/yqqxcxcampsm_xwd640.jpg/bill-cosby-is-een-vrij-man-vijf-vragen-over-de-beslissing-van-de-rechter.jpg', points: 0},
+ {name: 'DonaldTrump',source: 'https://images0.persgroep.net/rcs/7Lx4ekG65vqHR6cfLMui2aRjItc/diocontent/208413025/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8', points: 0},
+ {name: 'BarrackObama', source: 'https://www.europa-nu.nl/9353000/1/j4nvh0qavhjkdqd_j9vvj9idsj04xr6/vlbjj03d1wny?sizew=933&sizeh=1200&crop=778:58:1081:1390&lm=qfo8io', points: 0},
+ {name: 'JoeBiden', source: 'https://upload.wikimedia.org/wikipedia/commons/6/68/Joe_Biden_presidential_portrait.jpg', points: 0},
+ {name: 'JoeExotic', source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Joe_Exotic_%28Santa_Rose_County_Jail%29.png/266px-Joe_Exotic_%28Santa_Rose_County_Jail%29.png', points: 0},
+ {name: 'HillaryClinton', source: 'https://ichef.bbci.co.uk/news/1024/branded_news/1683A/production/_120681229_hillaryclintonatqueensbelfast005.jpg', points: 0},
+ {name: 'GeorgeBush', source: 'https://images0.persgroep.net/rcs/XzH8uDIuWNNJynh-DQvEXmjLPXQ/diocontent/31665363/_crop/0/233/3496/1975/_fitwidth/763?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.8', points: 0},
+ {name: 'JohnKennedy', source: 'https://www.biography.com/.image/t_share/MTgwNDM2ODA3NjQzNTcyMDc2/gettyimages-97347150.jpg', points: 0},
+ {name: 'RobertOppenheimer', source: 'https://upload.wikimedia.org/wikipedia/commons/0/03/JROppenheimer-LosAlamos.jpg', points: 0},
+ {name: 'TheodoreRoosevelt ', source: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/President_Roosevelt_-_Pach_Bros.jpg', points: 0},
+ {name: 'HarryPotter', source: 'https://upload.wikimedia.org//wikipedia/en/d/d7/Harry_Potter_character_poster.jpg', points: 0}
 ];
-startKnop.onclick = function () {
-    startPagina.style.display = "none"; //verander terug naar none
-    instellingenPagina.style.display = "none";
-    geschiedenisPagina.style.display = "none";
-    blindeVlekPagina.style.display = "none";
-    smoelenTrainer.style.display = "block";
+
+function displayAllHidden(){
+    //give everything hidden
+    instellingenPagina.classList.add("hidden");
+    smoelenTrainer.classList.add("hidden");
+    startPagina.classList.add("hidden");
+    geschiedenisPagina.classList.add("hidden");
+    blindeVlekPagina.classList.add("hidden");
+
+    instellingenPagina.classList.remove("shown");
+    smoelenTrainer.classList.remove("shown");
+    startPagina.classList.remove("shown");
+    geschiedenisPagina.classList.remove("shown");
+    blindeVlekPagina.classList.remove("shown");
+}
+
+backButtons.forEach(backButton => backButton.onclick = () => switchDisplay());
+
+startButton.onclick = () => {
+    displayAllHidden() 
+    smoelenTrainer.classList.remove("hidden");
+    smoelenTrainer.classList.add("shown");
     gridItems()
-    time()
+    setGameTimeCounter()
 }
 
-instellingenKnop.onclick = function () {
-    startPagina.style.display = "none"; //verander terug naar none
-    smoelenTrainer.style.display = "none";
-    instellingenPagina.style.display = "block";
-    geschiedenisPagina.style.display = "none";
-    blindeVlekPagina.style.display = "none";
+settingsButton.onclick = () => {
+    displayAllHidden() 
+    instellingenPagina.classList.remove("hidden");
+    instellingenPagina.classList.add("shown");
 }
 
-geschiedenisKnop.onclick = function () {
-    startPagina.style.display = "none"; //verander terug naar none
-    smoelenTrainer.style.display = "none";
-    instellingenPagina.style.display = "none";
-    blindeVlekPagina.style.display = "none";
-    geschiedenisPagina.style.display = "block";
-    document.getElementById('matchGeschiedenis').innerHTML = "";
-    for (let i = 0; i < scoreGeschiedenis.length; i++) {
+historyButton.onclick = () => {
+    displayAllHidden() 
+    geschiedenisPagina.classList.remove("hidden");
+    geschiedenisPagina.classList.add("shown");
+
+    document.getElementById('matchHistory').innerHTML = "";
+    for (let i = 0; i < scoreHistory.length; i++) {
         if (i <= 10) {
             var p = document.createElement("p");
-            p.innerHTML = "Juiste antwoorden " + scoreGeschiedenis[i].Goed +" - "+ " Onjuiste antwoorden " + scoreGeschiedenis[i].Fout + " - " + scoreGeschiedenis[i].tijd;
-            document.getElementById('matchGeschiedenis').appendChild(p)         
+            p.innerHTML = "Juiste antwoorden " + scoreHistory[i].Goed +" - "+ " Onjuiste antwoorden " + scoreHistory[i].Fout + " - " + scoreHistory[i].tijd;
+            document.getElementById('matchHistory').appendChild(p)         
         }
     }
 }
 
-blindeVlekKnop.onclick = function(){
-    startPagina.style.display = "none"; //verander terug naar none
-    smoelenTrainer.style.display = "none";
-    instellingenPagina.style.display = "none";
-    geschiedenisPagina.style.display = "none";
-    blindeVlekPagina.style.display = "block";
+blindeVlekButton.onclick = () => {
+    displayAllHidden() 
+    blindeVlekPagina.classList.remove("hidden");
+    blindeVlekPagina.classList.add("shown");
+
    document.getElementById("blindeVlekGeschiedenis").innerHTML = "";
-    for (let i = 0; i < blindeVlekPunten.length; i++) {
-        blindeVlekPunten[i].sort((a,b) => (a.punten < b.punten) ? 1 : ((b.punten < a.punten) ? -1 : 0));
-        if (blindeVlekPunten != 0 && i <= 5){
-            console.log(blindeVlekPunten[i].length)
-                for (let index = 0; index < 3; index++) {
-                    var p = document.createElement("p");
-                    p.innerHTML = "Meeste fouten antwoorden " + blindeVlekPunten[i][index].name + " Aantal fouten punten " + blindeVlekPunten[i][index].punten;
-                    document.getElementById('blindeVlekGeschiedenis').appendChild(p)      
-                }     
-            }        
-        }
+    for (let i = 0; i < blindeVlekPoints.length; i++) {
+        blindeVlekPoints[i].sort((a,b) => (a.points < b.points) ? 1 : ((b.points < a.points) ? -1 : 0));
+        if (blindeVlekPoints != 0 && i <= 5){
+            for (let index = 0; index < 3; index++) {
+                var p = document.createElement("p");
+                p.innerHTML = "Meeste fouten antwoorden " + blindeVlekPoints[i][index].name + " Aantal fouten points " + blindeVlekPoints[i][index].points;
+                document.getElementById('blindeVlekGeschiedenis').appendChild(p)      
+            }     
+        }        
     }
-
-terugKnop1.onclick = function () {
-    startPagina.style.display = "block"; //verander terug naar none
-    smoelenTrainer.style.display = "none";
-    instellingenPagina.style.display = "none";
-    geschiedenisPagina.style.display = "none";
-    blindeVlekPagina.style.display = "none";
 }
 
-terugKnop2.onclick = function () {
-    startPagina.style.display = "block"; //verander terug naar none
-    smoelenTrainer.style.display = "none";
-    instellingenPagina.style.display = "none";
-    geschiedenisPagina.style.display = "none";
-    blindeVlekPagina.style.display = "none";
-}
-
-terugKnop3.onclick = function () {
-    startPagina.style.display = "block"; //verander terug naar none
-    smoelenTrainer.style.display = "none";
-    instellingenPagina.style.display = "none";
-    geschiedenisPagina.style.display = "none";
-    blindeVlekPagina.style.display = "none";
-
+function switchDisplay(){
+    displayAllHidden() 
+    startPagina.classList.remove("hidden");
+    startPagina.classList.add("shown");
 }
 
 function gridItems() {
-    var persoonData = getRadioBtn();
+    var persoonData = getPlayerCount();
 
 
     for (let index = 0; index < persoonData; index++ ) {
@@ -111,41 +102,37 @@ function gridItems() {
         var fotoDiv = document.createElement("div");
         var foto = document.createElement("img");
 
-        naam.innerHTML = fotoPersoon[index].name;
-        foto.src = fotoPersoon[index].source;
+        naam.innerHTML = persons[index].name;
+        foto.src = persons[index].source;
  
-        naam.id = fotoPersoon[index].name;
-        fotoDiv.id = fotoPersoon[index].name;
+        naam.id = persons[index].name;
+        fotoDiv.id = persons[index].name;
 
         naam.classList.add("gridItem")
         fotoDiv.classList.add("gridItem")
 
         fotoDiv.appendChild(foto);
 
-        naamArray.push(naam);
-        fotoArray.push(fotoDiv);
+        names.push(naam);
+        photos.push(fotoDiv);
     }
-    naamArray.sort(() => Math.random() - 0.5);
-    fotoArray.sort(() => Math.random() - 0.5);
+    names.sort(() => Math.random() - 0.5);
+    photos.sort(() => Math.random() - 0.5);
 
-    for (let i = 0; i < naamArray.length; i++) {
-        gridContainer1.appendChild(naamArray[i]);
-        gridContainer2.appendChild(fotoArray[i]);
+    for (let i = 0; i < names.length; i++) {
+        gridContainer1.appendChild(names[i]);
+        gridContainer2.appendChild(photos[i]);
 
-
-        //run functie "itemOnclick"
-        //geef parameter [index] mee (Bijv op for loop 1 geeft hij "0" mee) 
-        itemOnclick(naamArray[i], fotoArray[i]);
+        names[i].onclick = () => checkClick(names[i])
+        photos[i].onclick = () => checkClick(photos[i]) 
     }
 }
 
-function matchGeschiedenis(){
-    scoreGeschiedenis.unshift({Goed: goedCount , Fout: foutCount, tijd: Date(), meesteFout: ""});
-    console.log(scoreGeschiedenis);
-
+function matchHistory(){
+    scoreHistory.unshift({Goed: correctAnswersCounter , Fout: incorrectAnswersCounter, tijd: Date()});
 }
 
-function time(){
+function setGameTimeCounter(){
     var timeleft = document.getElementById("userInput").value;
     if (!timeleft) { timeleft = 10; }
 
@@ -154,34 +141,37 @@ function time(){
         clearInterval(downloadTimer);
         document.getElementById("countdown").innerHTML = "tijd is om";
         setTimeout(() => {
-            matchGeschiedenis()
-            startPagina.style.display = "block"; //verander terug naar none
-            smoelenTrainer.style.display = "none";
+            matchHistory()
+            displayAllHidden() 
+
+            startPagina.classList.remove("hidden");
+            startPagina.classList.add("shown");
+
             gridContainer1.innerHTML = "";
             gridContainer2.innerHTML = "";
             document.getElementById("pointsGoed").innerHTML = "";
             document.getElementById("pointsFout").innerHTML = "";
-            naamArray = [];
-            fotoArray = [];
-            foutCount = 0;
-            goedCount = 0;
+            names = [];
+            photos = [];
+            incorrectAnswersCounter = 0;
+            correctAnswersCounter = 0;
 
             var tempArray = [];
 
-            for (let i = 0; i <  getRadioBtn(); i++) {
-                tempArray.push({name: fotoPersoon[i].name, punten: fotoPersoon[i].punten});      
+            for (let i = 0; i <  getPlayerCount(); i++) {
+                tempArray.push({name: persons[i].name, points: persons[i].points});      
             }
-           blindeVlekPunten.unshift(tempArray);
+           blindeVlekPoints.unshift(tempArray);
 
-            for (let index = 0; index < fotoPersoon.length; index++) {
-                fotoPersoon[index].punten = 0;
+            for (let index = 0; index < persons.length; index++) {
+                persons[index].points = 0;
                 
             }
             
 
 // maak een lege array aan  boven de for loop en tijdens de eerste for loop vult die blindebelekkepunten.
-// vul die met de punten 
-// daarna gaat die met een andere for loop de  array fotopersoon leeg maken maar de lege array boven aan staat dan nog vol met de punten.
+// vul die met de points 
+// daarna gaat die met een andere for loop de  array fotopersoon leeg maken maar de lege array boven aan staat dan nog vol met de points.
 
 
         }, 3000);
@@ -192,34 +182,23 @@ function time(){
     }, 1000);
 }
 
-function itemOnclick(naamNummer, fotoNummer){
-
-    naamNummer.onclick = function () { checkClick(this); }
-    fotoNummer.onclick = function () { checkClick(this); }
-}
-
 function checkClick(parameter){
     if (lastClick) {
         //Kijk of de cijfers gelijk zijn van lastchar en lastchar prev
         //Kijk of het div element hetzelfde is van lastClick en parameter
         if (lastClick.id == parameter.id && lastClick != parameter) {
-            console.log("Het is een match")
-            parameter.style.display = "none";
-            lastClick.style.display = "none";
+            parameter.classList.add("hidden");
+            lastClick.classList.add("hidden");
             lastClick = "";
-            goedCount = goedCount + 1;
-            document.getElementById("pointsGoed").innerHTML = goedCount + "Heeft u er goed";
+            correctAnswersCounter = correctAnswersCounter + 1;
+            document.getElementById("pointsGoed").innerHTML = correctAnswersCounter + "Heeft u er goed";
         }
         else{
-            
-            console.log('Hij is fout ', parameter.id);
             lastClick = "";
-            foutCount = foutCount + 1;
-            document.getElementById("pointsFout").innerHTML = foutCount  + "Heeft u er fout";
-            var result = fotoPersoon.find( ({ name }) => name === parameter.id);
-            result.punten = (result.punten + 1);
-            console.log(fotoPersoon);
-
+            incorrectAnswersCounter = incorrectAnswersCounter + 1;
+            document.getElementById("pointsFout").innerHTML = incorrectAnswersCounter  + "Heeft u er fout";
+            var result = persons.find( ( name ) => name === parameter.id);
+            result.points = (result.points + 1);
         }
     }
     else{
@@ -228,15 +207,24 @@ function checkClick(parameter){
     }
 }
 
-function getRadioBtn(){
+function getPlayerCount(){
     var radios = document.getElementsByName('spelerAantal');
+    var playerCount = 5;
 
     for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
-            return radios[i].value;
-        }
-        else{
-           return 5;
+            playerCount = radios[i].value;
         }
     }
+
+    // radios.forEach(radio => radio.checked ? playerCount = radio.value : null)
+
+    // radios.forEach(radio => {
+    //     if (radio.checked) {
+    //         playerCount = radio.value;
+    //     }
+    // })
+
+    // return 5 wordt uitgevoerd als die tijdens de for loop van radios geen input vind.
+    return playerCount;
 }
